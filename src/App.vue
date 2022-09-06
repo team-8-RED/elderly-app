50f3ec2923b0f95f48bc53fbbdb0c7c6cc89b802
Tomoki Aizawa <aiaiaiaidayo4412@gmail.com>
2022-09-06 12:57:46 +0900

    フッターを作りました

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
+  <Header />
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view />
+  <Footer />
 </template>
+<script>
+import Header from "./components/HeaderComponent.vue"
+import Footer from "./components/FooterComponent.vue"
+
+export default {
+  components: { Header, Footer },
+}
+</script>
 
 <style>
 #app {

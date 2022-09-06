5cc6bab3542c7c927be5cf3fab7a5a4711dc4b91
t203037 <t203037@edu.kaiyodai.ac.jp>
2022-09-06 20:46:12 +0900

    practice

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,16 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
-const firebaseConfig = {}
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBPzivzdRx20fhgHskQ7G2U7MkuXyz7KRU",
+  authDomain: "eldeely-app.firebaseapp.com",
+  projectId: "eldeely-app",
+  storageBucket: "eldeely-app.appspot.com",
+  messagingSenderId: "199594086984",
+  appId: "1:199594086984:web:5cc2683e41ae826fc63e7d",
+  measurementId: "G-1YS88W9CTL",
+}
 
 const app = initializeApp(firebaseConfig)
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { createRouter, createWebHistory } from "vue-router"
+import HomeView from "../views/HomeView.vue"
 
 const routes = [
   {
@@ -14,13 +14,13 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+      import(/* webpackChunkName: "about" */ "../views/LInecourse.vue"),
   },
-];
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-});
+})
 
-export default router;
+export default router

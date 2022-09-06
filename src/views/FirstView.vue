951e76fca8acd03151d0053fea7b1a8e8e4250dd
Tomoki Aizawa <aiaiaiaidayo4412@gmail.com>
2022-09-06 15:24:46 +0900

    初めのページを作りました

---
+++ src/views/FirstView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="contents">
+    <div class="title-text">
+      <h2>スマホで</h2>
+      <h2>人生を豊かにしよう</h2>
+    </div>
+    <div class="button-wrapper">
+      <button class="start-button">ログイン</button>
+      <button class="start-button">新規登録</button>
+    </div>
+    <div class="site-image">
+      <img src="" alt="サイトイメージ" />
+    </div>
+  </div>
+</template>
+
+<style>
+.contents {
+  display: flex;
+  flex-direction: column;
+}
+.title-text {
+  padding: 20px;
+  text-align: center;
+}
+.site-image {
+  background-color: #e1eec1;
+  height: 30vh;
+  background-size: cover;
+}
+.start-button {
+  background-color: #7ab977;
+  color: #fff;
+  margin: 0.25rem;
+  margin-bottom: 30px;
+}
+</style>
